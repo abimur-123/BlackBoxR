a06e39b2a426869ab91965014a8e53875c074c17
Vinver Guan <353164374@qq.com>
2018-03-02 17:58:42 -0800

    add documentation for test function

--- tests/testthat/test-getCredibleInterval.R
+++ tests/testthat/test-getCredibleInterval.R
@@ -1,10 +1,11 @@
 context('getCredibleInterval.R')
 
 
+#sample dataframe
 df<-data.frame(name=rep('A',4),score=rbeta(4,3,2))
 
 
-
+#check valid input
 test_that("check if input is in correct format",{
   expect_error(getCredibleInterval(1,2),'Wrong format for input')
   expect_error(getCredibleInterval(), 'need to pass in a dataframe')
@@ -18,6 +19,8 @@ test_that('the second column of dataframe is numeric',{
   expect_true(typeof(df[,2])=='double'|typeof(df[,2])=='integer')
 })
 
+
+#check valid output
 test_that('the output interval is valid',{
   expect_true(getCredibleInterval(df)$lower>=min(df$score))
   expect_true(getCredibleInterval(df)$upper<=max(df$score))
@@ -28,6 +31,8 @@ test_that('the output interval is correct',{
   expect_equal(getCredibleInterval(df)$lower,expected_interval[1])
   expect_equal(getCredibleInterval(df)$upper,expected_interval[2])
 })
+
+
 
 
   

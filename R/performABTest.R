2c7026df976be9af3146302b81fe96152a2e0f3d
Mojo <abimur@gmail.com>
2018-03-03 18:07:51 -0800

    Add AB test functions

---
+++ R/performABTest.R
@@ -0,0 +1,15 @@
+#performABtest_freq
+
+#' @export
+performABtest_freq <- function(data){
+  #perform AB testing using the frequentist approach
+  #
+  #Args:
+  #data: dataframe which consists of events and results
+  #
+  #Return:
+  #results containing significance of tests
+  #
+  return(NULL)
+  
+}
---
+++ R/performABTest_Bayesian.R
@@ -0,0 +1,15 @@
+#performABtest_Baye
+
+#' @export
+performABtest_Baye <- function(data){
+  #perform AB testing using the Bayesian approach
+  #
+  #Args:
+  #data: dataframe which consists of events and results
+  #
+  #Return:
+  #results containing significance of tests
+  #
+  return(NULL)
+  
+}

b371dc6bc647da9749e3ed32efcfb30bd27445cc
Mojo <abimur@gmail.com>
2018-03-16 15:23:45 -0700

    Try installing ggplot2

---
+++ Installations.R
@@ -0,0 +1,13 @@
+#!/usr/bin/Rsript
+# ---------------------------------------------------------
+# Filename      : Installations.R
+# Authors       : Abishek Murali
+# Description   : Pre-Installation libraries
+# Credit script : https://github.com/IsmailM/R-Core/blob/master/Installations.R
+# ---------------------------------------------------------
+
+#############################################################################
+#       Load necessary dependancies, if not previously installed            #
+#############################################################################
+
+devtools::install_github("tidyverse/ggplot2")
